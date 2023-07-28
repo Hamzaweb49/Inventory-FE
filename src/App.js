ad0e375278da273c8a491d8bf7a927d685ba78eb
Hamza <developerhamza29@gmail.com>
2023-07-28 14:38:12 +0500

    Initial Comit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import ForgotPassword from "./pages/ForgotPassword";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Routes, Route } from "react-router-dom";
+import TwoFactorAuth from "./pages/TwoFactorAuth";
+import ResetPassword from "./pages/ResetPassword";
+import ValidateOTP from "./pages/ValidateOTP";
+import PrivateRoute from "./components/HOCs/PrivateRoute";
+import Settings from "./pages/Settings";
+import PersonalInfo from "./pages/PersonalInfo";
+import ProfileForm from "./pages/ProfileForm";
+
+function App() {
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" exact element={<PrivateRoute />}>
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="/Settings" exact element={<Settings />} />
+          <Route
+            path="/Personal-information"
+            exact
+            element={<PersonalInfo />}
+          />
+          <Route path="/Profile-form" exact element={<ProfileForm />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/enable-2fa" element={<TwoFactorAuth />} />
+        <Route path="/2fa" element={<ValidateOTP />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
